79a56bd08d08bc68fac5f75a10b9336eb55e5972
junga <gop3064@naver.com>
2020-03-17 21:27:31 +0900

    백준 1987 알파벳 속도 개선

--- Bj1987.py
+++ Bj1987.py
@@ -1,26 +1,28 @@
 # 1987 알파벳 https://www.acmicpc.net/problem/1987
-# 145768 KB, 5112 ms (PyPy3)
+# 148900 KB, 4788 ms (PyPy3)
 
 def charToNum(c):
     return ord(c) - ord('A')
 
-def next(x, y, cnt):
+def isAnswer(cnt):
     global answer
-    ctn = ''
-    check = False
+    if cnt > answer:
+        answer = cnt
+
+def next(x, y, cnt):
     if 0 <= x < r and 0 <= y < c:
         ctn = charToNum(alphabets[x][y])
-        check = True
-    if check and not isUsed[ctn]:
-        isUsed[ctn] = True
-        next(x, y+1, cnt+1)
-        next(x, y-1, cnt+1)
-        next(x-1, y, cnt+1)
-        next(x+1, y, cnt+1)
-        isUsed[ctn] = False
+        if not isUsed[ctn]:
+            isUsed[ctn] = True
+            next(x, y+1, cnt+1)
+            next(x, y-1, cnt+1)
+            next(x-1, y, cnt+1)
+            next(x+1, y, cnt+1)
+            isUsed[ctn] = False
+        else:
+            isAnswer(cnt)
     else:
-        if cnt > answer:
-            answer = cnt
+        isAnswer(cnt)
 
 if __name__ == '__main__':
     r, c = map(int, input().split())  # 1 <= r, c <= 20
