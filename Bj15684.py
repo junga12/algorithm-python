2f123f071208fd1b9db3cb6840548b70520605a2
junga <gop3064@naver.com>
2020-04-17 16:40:29 +0900

    fixup! 백준 15684 사다리 조작 속도 개선

--- Bj15684.py
+++ Bj15684.py
@@ -1,6 +1,7 @@
 # 15684 사다리 조작 https://www.acmicpc.net/problem/15684
+# 123720 KB, 1392 ms (PyPy3)
 
-def check():
+def check(): # 출력 조건오 부합하는지 검사
     for i in range(1, n+1):
         cur = i
         for j in range(1, h+1):
@@ -12,49 +13,30 @@ def check():
             return False
     return True
 
-def dfs(count, target_num): # count: 현재 개수, target_num: 추가할 선의 개수
+def dfs(curi, count, target_num): # count: 현재 개수, target_num: 추가할 선의 개수
     global answer
     if answer != -1:
         return
-
     if count == target_num:
         if check():
             answer = count
         return
-
-    for i in range(1, h+1): # Todo: to next
+    for i in range(curi, h+1):
         for j in range(1, n):
-            if not ladder[i][j]: # and not ladder[i][j-1]:
-                if j > 1 and ladder[i][j-1]: # Todo: edit to better way
-                    continue
-                if j+1 < n and ladder[i][j+1]:
-                    continue
+            if not ladder[i][j] and not ladder[i][j-1] and not ladder[i][j+1]:
                 ladder[i][j] = True
-                dfs(count+1, target_num)
+                dfs(i, count+1, target_num)
                 ladder[i][j] = False
-
-
-def printLadder():
-    print("="*50)
-    for i in ladder[1:]:
-        for j in i:
-            if j:
-                print('O', end=' ')
-            else:
-                print('X', end=' ')
-        print()
 
 if __name__ == '__main__':
     n, m, h = map(int, input().split()) # 2 <= n <= 10, 0 <= m <= (n-1)*h, 1 <= h <= 30
-    ladder = [[0] * n] + [[False] * n for _ in range(h)] # 가로선
+    ladder = [[False] * (n+1) for _ in range(h+1)] # 가로선
     for _ in range(m):
         a, b = map(int, input().split()) # 1 <= a <= h, 1 <= b <= n-1
         ladder[a][b] = True
-
-    MAX_NUM = 3
     answer = -1
-    for i in range(MAX_NUM+1): # 추가할 선의 개수
-        dfs(0, i)
+    for i in range(4):
+        dfs(1, 0, i)
         if (answer != -1):
             break
     print(answer)
